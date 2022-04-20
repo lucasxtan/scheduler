3e01bab15b57314ea52a8a19be843c72a1c86346
Lucas Tan <sheng.x.tan@gmail.com>
2022-04-19 22:39:52 -0600

    imported classNames to button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,26 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
    let buttonClass = "button";
- 
+
    if (props.confirm) {
-     buttonClass += " button--confirm";
+      buttonClass += " button--confirm";
    }
- 
-   return <button className={buttonClass}>{props.children}</button>;
- }
+
+   else if (props.danger) {
+      buttonClass += " button--danger";
+   }
+
+   return (
+      <button
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   );
+}
